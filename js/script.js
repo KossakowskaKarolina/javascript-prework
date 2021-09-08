a2e24425ce1fb0444046bf704a4b6af042378079
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2021-09-08 21:07:37 +0200

    Change let to const and named functions to anonymous

--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
-function playGame(playerInput){
+{
+const playGame = function(playerInput){
 
   clearMessages()
   
-  function getMoveName(argMoveId){
+  const getMoveName = function(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
     } else if(argMoveId == 2){
       return 'papier';
     } else if(argMoveId == 3){
       return 'nożyce';
-    } else {
-      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-      return 'nieznany ruch';
-    }
+    } 
   }
 
 
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
 
 
   console.log('Wylosowana liczba to: ' + randomNumber);
 
-  let argComputerMove = getMoveName(randomNumber);
+  const argComputerMove = getMoveName(randomNumber);
 
   printMessage('Mój ruch to: ' + argComputerMove);
 
 
   console.log('Gracz wpisał: ' + playerInput);
 
-  let argPlayerMove = getMoveName(playerInput);
+  const argPlayerMove = getMoveName(playerInput);
 
   printMessage('Twój ruch to: ' + argPlayerMove);
 
-
-  function displayResult(argComputerMove, argPlayerMove){
+  const displayResult = function(argComputerMove, argPlayerMove){
     console.log('moves:', argComputerMove, argPlayerMove);
     if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
       printMessage('Ty wygrywasz!');
@@ -43,8 +40,6 @@ function playGame(playerInput){
       printMessage('Ty wygrywasz!');
     } else if(argComputerMove == argPlayerMove){
       printMessage('Remis!');
-    } else if(argPlayerMove == 'nieznany ruch'){
-      printMessage('Możesz wybrać tylko spośród 1, 2 lub 3! Wybierz ponownie swój ruch.');
     } else{
       printMessage('Ty przegrywasz!');
     }
@@ -65,3 +60,4 @@ document.getElementById('play-paper').addEventListener('click', function(){
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame(3);
 });
+}
